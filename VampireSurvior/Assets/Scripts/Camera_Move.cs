0c419d1d1813428931c17a75cd9d108466efdc4f
Nayeansoo <nyslove0408@gmail.com>
2026-01-15 09:36:53 +0900

    20260115

---
+++ VampireSurvior/Assets/Scripts/Camera_Move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Camera_Move : MonoBehaviour
+{
+    public Transform target; //타겟
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position; //카메라의 위치를 타겟의 위치로 한다
+        
+    }
+}
---
+++ VampireSurvior/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //가로입력
+        float v = Input.GetAxis("Vertical") * speed * Time.deltaTime; //세로입력
+
+        transform.position += new Vector3(-v, 0, h);
+    }
+}
